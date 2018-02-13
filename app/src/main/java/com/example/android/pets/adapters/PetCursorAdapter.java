91c540f51a0912adb7e39d4891e1f738c36f3b86
anega <anegamega@gmail.com>
2018-02-13 09:11:18 +0200

    Add PetCursorAdapter

---
+++ app/src/main/java/com/example/android/pets/adapters/PetCursorAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.pets.adapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.R;
+import com.example.android.pets.data.PetContract;
+
+/**
+ * {@link PetCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
+public class PetCursorAdapter extends CursorAdapter {
+    /**
+     * Constructs a new {@link PetCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param viewGroup  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflated template
+        TextView tvPetName = (TextView) view.findViewById(R.id.tv_main_petname);
+        TextView tvPetBreed = (TextView) view.findViewById(R.id.tv_main_petbreed);
+        // Extract properties from cursor
+        String petName = cursor.getString(cursor.getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_NAME));
+        String petBreed = cursor.getString(cursor.getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_BREED));
+        // Populate fields with extracted properties
+        tvPetName.setText(petName);
+        tvPetBreed.setText(petBreed);
+    }
+}
